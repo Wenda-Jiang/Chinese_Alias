e4c7c1bd588cf21c3950b70088f254f7ba5b6d67
wenda <wenda@mogujie.com>
2018-03-08 10:34:18 +0800

    fomat code

--- gen_alias/lstm_alias.py
+++ gen_alias/lstm_alias.py
@@ -95,10 +95,17 @@ class Alias(object):
     return output, state
 
   def decode_zi(self):
+    state = self.state
 
     if self.mode == 'train':
-      outputs, state = tf.nn.static_rnn(self.cell, tf.unstack(self.zi_emb, axis=1), initial_state=self.state)
-      logits = tf.layers.dense(tf.stack(outputs, axis=0), term_size, name="logits")
+      decode_input = tf.unstack(self.zi_emb, axis=1)
+
+      outputs = []
+      for inp in decode_input:
+        output, state = self.decode_step(inp, state, reuse=False if len(outputs) == 0 else True)
+        outputs.append(output)
+
+      logits = tf.layers.dense(tf.transpose(outputs, [1, 0, 2]), term_size, name="logits")
       losses = tf.nn.sparse_softmax_cross_entropy_with_logits(
         labels=self.target, logits=logits)
 
@@ -106,12 +113,12 @@ class Alias(object):
       tf.losses.add_loss(self.batch_loss)
 
     else:
-
-      output, state = self.decode_step(self.zi_emb, self.state, reuse=False)
-      logit = tf.layers.dense(output, term_size, name="logits")
-      values_first, indices_first = tf.nn.top_k(logit, k=3)
-      seconds_i = tf.unstack(indices_first, axis=1, num=3)
-      seconds_v = tf.unstack(values_first, axis=1, num=3)
+      beam_size = 4
+      output, state = self.decode_step(self.zi_emb, self.state)
+      logit = tf.nn.softmax(tf.layers.dense(output, term_size, name="logits"))
+      values_first, indices_first = tf.nn.top_k(logit, k=beam_size)
+      seconds_i = tf.unstack(indices_first, axis=1, num=beam_size)
+      seconds_v = tf.unstack(values_first, axis=1, num=beam_size)
 
       self.results_i = []
       self.results_v = []
@@ -119,8 +126,8 @@ class Alias(object):
       for inp, inp_value in zip(seconds_i, seconds_v):
         values_emb = tf.nn.embedding_lookup(self.word_emb, inp)
         o2, s2 = self.decode_step(values_emb, state, reuse=False)
-        l2 = tf.layers.dense(o2, term_size, name="logits", reuse=True)
-        v2, i2 = tf.nn.top_k(l2, k=3)
+        l2 = tf.nn.softmax(tf.layers.dense(o2, term_size, name="logits", reuse=True))
+        v2, i2 = tf.nn.top_k(l2, k=beam_size)
         for i, v in zip(tf.unstack(i2, axis=1), tf.unstack(v2, axis=1)):
           self.results_i.append(tf.squeeze(tf.stack([inp, i])))
           self.results_v.append(tf.squeeze(tf.stack([inp_value, v])))
